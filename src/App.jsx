49170fd6b8ae383028c893c2b147b02c0d76b348
cuongtran1201-zzios <cuongtvhe181518@gmail.com>
2026-07-31 17:16:57 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -359,7 +359,7 @@ export default function App() {
       
       <Toast message={toast} />
       <ScrollToTop />
-      <ChatWidget />
+      <ChatWidget customer={customer} />
     </div>
   );
 }
