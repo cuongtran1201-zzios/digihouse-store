731b33ea8a5c18ce7b5ee8de087d1ef072a81378
cuongtran1201-zzios <cuongtvhe181518@gmail.com>
2026-08-03 01:14:45 +0700

    Update and rename Contactconfig.js to contactConfig.js

--- src/data/contactConfig.js
+++ src/data/contactConfig.js
@@ -2,6 +2,6 @@
 export const CONTACT_INFO = {
   phone: '0911581581',              // Số điện thoại/hotline
   messengerPageId: 'digihouse.vn',  // Username/Page ID Facebook (lấy từ link fb.com/xxx)
-  instagramUsername: 'digihouse.vn', // Username Instagram (không có dấu @)
-  tiktokUsername: 'digihouse.vn',    // Username TikTok (không có dấu @)
+  instagramUsername: '_digihouse.vn', // Username Instagram (không có dấu @)
+  tiktokUsername: 'DigiHouse - Digital Camera',    // Username TikTok (không có dấu @)
 };
